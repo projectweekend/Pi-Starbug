6a6ac113afa2fd19da91fc8a124b06bf8ab3aa08
Brian Hines <brian@projectweekend.net>
2014-01-26 19:09:01 -0600

    Making things a little smarter...more to come

--- indoor_temperature_humidity.py
+++ indoor_temperature_humidity.py
@@ -4,6 +4,12 @@ import dhtreader
 SENSOR_TYPE = 22
 SENSOR_PIN = 4
 
-dhtreader.init()
-print dhtreader.read(SENSOR_TYPE, SENSOR_PIN)
 
+def worker():
+    dhtreader.init()
+    result = dhtreader.read(SENSOR_TYPE, SENSOR_PIN)
+    return result
+
+
+if __name__ == "__main__":
+    worker()
