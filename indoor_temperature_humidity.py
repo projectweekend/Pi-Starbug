35f21a832301b2464d28b4f77b7b6003d6e31298
Brian Hines <brian@projectweekend.net>
2014-01-27 14:57:13 -0600

    Make worker stash data to be pick up later

--- indoor_temperature_humidity.py
+++ indoor_temperature_humidity.py
@@ -1,19 +1,50 @@
+import time
+import datetime
+
 import dhtreader
-import holly
+import utils
+from stash import Stash
 
 
 TEMPERATURE_SENSOR_TYPE = 22
-TEMPERATURE_SENSOR_PIN = 4    
+TEMPERATURE_SENSOR_PIN = 4
+
+
+def get_sensor_data():
+    return dhtreader.read(TEMPERATURE_SENSOR_TYPE, TEMPERATURE_SENSOR_PIN)
+
+
+def stash_temperature(temperature_value):
+    stash = Stash("indoor_temperature")
+    temperature_data = {
+        'date': datetime.datetime.utcnow().isoformat(),
+        'celsius': temperature_value,
+        'fahrenheit': utils.celsius_to_fahrenheit(temperature_value)
+    }
+    stash.add(temperature_data)
+
+
+def stash_humidity(humidity_value):
+    stash = Stash("indoor_humidity")
+    humidity_data = {
+        'date': datetime.datetime.utcnow().isoformat(),
+        'percent': humidity_value
+    }
+    stash.add(humidity_data)
 
 
 def worker():
     dhtreader.init()
-    result = dhtreader.read(TEMPERATURE_SENSOR_TYPE, TEMPERATURE_SENSOR_PIN)
-    if result:
-        holly.send_temperature(result[0])
-        holly.send_humidity(result[1])
-        print "Result was empty"
-    return
+    # try this multiple times because we don't always get data on first attempt
+    for x in range(10):
+        result = get_sensor_data()
+        if result:
+            # stash the data we found and exit
+            stash_temperature(result[0])
+            stash_humidity(result[1])
+            break
+        # wait 10 seconds before attempting another sensor reading
+        time.sleep(10)
 
 
 if __name__ == "__main__":
