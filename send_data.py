169ada8b439a9ce71008a755509445a4fccf8222
Brian Hines <brian@projectweekend.net>
2014-02-28 10:13:23 -0600

    Don't attempt to send data if there is no data

--- send_data.py
+++ send_data.py
@@ -4,23 +4,26 @@ from stash import Stash
 
 def process_temperature_data():
     temperature_stash = Stash("indoor_temperature")
-    data_created = holly.send_bulk_temperature_data(temperature_stash.data)
-    if data_created:
-        temperature_stash.empty()
+    if temperature_stash.data:
+        data_created = holly.send_bulk_temperature_data(temperature_stash.data)
+        if data_created:
+            temperature_stash.empty()
 
 
 def process_humidity_data():
     humidity_stash = Stash("indoor_humidity")
-    data_created = holly.send_bulk_humidity_data(humidity_stash.data)
-    if data_created:
-        humidity_stash.empty()
+    if humidity_stash.data:
+        data_created = holly.send_bulk_humidity_data(humidity_stash.data)
+        if data_created:
+            humidity_stash.empty()
 
 
 def process_system_temperature_data():
     system_temperature_stash = Stash("system_temperature")
-    data_created = holly.send_bulk_system_temperature_data(system_temperature_stash.data)
-    if data_created:
-        system_temperature_stash.empty()
+    if system_temperature_stash.data:
+        data_created = holly.send_bulk_system_temperature_data(system_temperature_stash.data)
+        if data_created:
+            system_temperature_stash.empty()
 
 
 def worker():
