fe497658fcc96e9d1dc2c51df7cd3a95ecb405dd
Brian Hines <brian@projectweekend.net>
2014-02-28 09:35:17 -0600

    Make separate process to collect system temperature and stash it...
    …this is the same pattern used by the indoor temp/humidity process

---
+++ collect_system_temperature.py
@@ -0,0 +1,34 @@
+import subprocess
+import datetime
+
+import utils
+from stash import Stash
+
+
+SHELL_COMMAND = ["/opt/vc/bin/vcgencmd", "measure_temp"]
+
+
+def get_system_temp():
+    system_result = subprocess.check_output(SHELL_COMMAND)
+    celsius_temp = utils.parse_temp_value(system_result)
+    fahrenheit_temp = utils.celsius_to_fahrenheit(celsius_temp)
+    current_date = datetime.datetime.utcnow().isoformat()
+    return {
+        'date': current_date,
+        'celsius':celsius_temp, 
+        'fahrenheit': fahrenheit_temp
+    }
+
+
+def stash_system_temperature(temperature_value):
+    system_temperature_stash = Stash("system_temperature")
+    system_temperature_stash.add(temperature_value)
+
+
+def worker():
+    system_temperature = get_system_temp()
+    stash_system_temperature(system_temperature)
+
+
+if __name__ == "__main__":
+    worker()
