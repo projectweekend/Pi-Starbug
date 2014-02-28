e459cb654e33a96e2ef199432bb264ff645060fd
Brian Hines <brian@projectweekend.net>
2014-02-28 08:49:30 -0600

    Refactor api_helpers to use Hammock

--- holly/api_helpers.py
+++ holly/api_helpers.py
@@ -1,25 +1,22 @@
-import utils
+import json
+from hammock import Hammock
+
+API = Hammock('http://holly.local/api')
 
 
 def send_bulk_humidity_data(humidity_data_list):
-    url = "http://holly.local/api/indoor/humidity/bulk"
-    post_data = {
-        'humidity_data': humidity_data_list
-    }
-    response = utils.make_json_post(url, post_data)
-    if response != 201:
+    post_data = json.dumps({'humidity_data': humidity_data_list})
+    response = API.indoor.humidity.bulk.POST(data=post_data)
+    if response.status_code != 201:
         # todo logging
         return False
     return True
 
 
 def send_bulk_temperature_data(temperature_data_list):
-    url = "http://holly.local/api/indoor/temperature/bulk" 
-    post_data = {
-        'temperature_data': temperature_data_list
-    }
-    response = utils.make_json_post(url, post_data)
-    if response != 201:
+    post_data = json.dumps({'temperature_data': temperature_data_list})
+    response = API.indoor.temperature.bulk.POST(data=post_data)
+    if response.status_code != 201:
         # todo logging
         return False
     return True
