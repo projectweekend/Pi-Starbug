5a531c68595ef12a32f537b7b93ed717d799ac81
Brian Hines <brian@projectweekend.net>
2014-02-28 11:04:12 -0600

    Make sure I set headers for JSON posts to Holly

--- holly/api_helpers.py
+++ holly/api_helpers.py
@@ -2,11 +2,12 @@ import json
 from hammock import Hammock
 
 API = Hammock('http://holly.local/api')
+API_HEADERS = {'Content-type': 'application/json'}
 
 
 def send_bulk_humidity_data(humidity_data_list):
     post_data = json.dumps({'humidity_data': humidity_data_list})
-    response = API.indoor.humidity.bulk.POST(data=post_data)
+    response = API.indoor.humidity.bulk.POST(data=post_data, headers=API_HEADERS)
     if response.status_code != 201:
         # todo logging
         return False
@@ -15,7 +16,7 @@ def send_bulk_humidity_data(humidity_data_list):
 
 def send_bulk_temperature_data(temperature_data_list):
     post_data = json.dumps({'temperature_data': temperature_data_list})
-    response = API.indoor.temperature.bulk.POST(data=post_data)
+    response = API.indoor.temperature.bulk.POST(data=post_data, headers=API_HEADERS)
     if response.status_code != 201:
         # todo logging
         return False
@@ -24,7 +25,7 @@ def send_bulk_temperature_data(temperature_data_list):
 
 def send_bulk_system_temperature_data(temperature_data_list):
     post_data = json.dumps({'temperature_data': temperature_data_list})
-    response = API.starbug.temperature.bulk.POST(data=post_data)
+    response = API.starbug.temperature.bulk.POST(data=post_data, headers=API_HEADERS)
     if response.status_code != 201:
         # todo logging
         return False
